6f03d824084727d1c299d5e1ccd430adcf05e572
sathishcharykotha <ksathish@stratapps.com>
2025-06-15 20:45:13 +0530

    Add basic chess game with Angular and Electron

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ChessGame } from './chess-game/chess-game';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: ChessGame }
+];
